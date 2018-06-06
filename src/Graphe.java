a8ee60b1111543c674a51bb405e885f95528363b
DylanMarsangy <dylanmarsangy@outlook.fr>
2018-06-06 22:19:34 +0200

    Methode pour chronométrer une fonction sur un graphe

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,130 +1,91 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Graphe {
-    public List<Sommet> getEnsembleSommet() {
-        return ensembleSommet;
-    }
 
-    protected String nom;
-    protected boolean oriente;
-    protected int nbSommet;
-    protected int nbValSommet;
-    protected int nbArcs;
-    protected int nbValArcs;
-
-    protected List<Sommet> ensembleSommet = new ArrayList<Sommet>();
+    private String nom;
+    private boolean oriente;
+    private int nbSommets;
+    private int nbValSommet;
+    private int nbArcs;
+    private int nbValArcs;
+    private List<Sommet> sommets = new ArrayList<>();
 
     public Graphe() {}
-    
-    public void lectureGraphe(String adresseFichier) throws IOException {
 
+    public void lectureGraphe(String adresseFichier) {
 
             InputStream in = null;
             BufferedReader br = null;
-            String line="";
-            int temp1,temp2;
-            int compteurLigne=0;
+            String line;
+            int temp1, temp2;
+            int compteurLigne = 0;
+            int compteurSommet = 0;
+
             try {
                 in = new FileInputStream(adresseFichier);
                 br = new BufferedReader(new InputStreamReader(in, "UTF-8"), 2048);
-                int position,i;
 
-                while ((line = br.readLine()) != null){
-                    position=line.indexOf(':')+1;
-                    line=line.substring(position);
-                    //On enlève les espaces inutiles
-                    for(i=0;line.charAt(i)==' ';i++);
+                int position, i;
+                while ((line = br.readLine()) != null) {
+                    position = line.indexOf(':')+1;
+                    line = line.substring(position);
+                    // On enlève les espaces inutiles
+                    for (i=0; line.charAt(i) == ' '; i++);
                     line = line.substring(i);
+
                     switch (compteurLigne) {
                         case 0:
-
-                            nom=line;
-
-
+                            nom = line;
                             break;
                         case 1:
-                            //On vérifie que notre graphe est orienté
-                            if(line.equalsIgnoreCase("oui")){
-                                oriente=true;
-
-
-                            }
-                            else{
-                                oriente=false;
-                            }
+                            oriente = line.equalsIgnoreCase("oui");
                             break;
                         case 2:
-                            nbSommet=Integer.parseInt(line);
-
+                            nbSommets = Integer.parseInt(line);
                             break;
                         case 3:
-                            nbValSommet=Integer.parseInt(line);
-
+                            nbValSommet = Integer.parseInt(line);
                             break;
                         case 4:
-                            nbArcs=Integer.parseInt(line);
-
+                            nbArcs = Integer.parseInt(line);
                             break;
                         case 5:
-                            nbValArcs=Integer.parseInt(line);
-
+                            nbValArcs = Integer.parseInt(line);
                             break;
-                        case 6:
-                            break;
-
-
                     }
-                    if (compteurLigne>6&&compteurLigne<7+nbSommet){
-                        //On va jusqu'a l'espace pour avoir le nom du sommet
-                        position=line.indexOf(' ')+1;
-                        line=line.substring(position);
 
-                        ensembleSommet.add(new Sommet(line));
-
+                    if (compteurLigne > 6 && compteurLigne < 7+ nbSommets) {
+                        // On va jusqu'à l'espace pour avoir le nom du sommet
+                        position = line.indexOf(' ') + 1;
+                        line = line.substring(position);
+                        sommets.add(new Sommet(compteurSommet, line));
+                        compteurSommet++;
                     }
+
                     compteurLigne++;
-
-                    if(compteurLigne>7+nbSommet+1&&compteurLigne<7+nbSommet+2+nbArcs){
-                        //On trouve l'espace entre les 2 chiffres
-
-                        temp1=Integer.parseInt(line.substring(0,line.indexOf(' ')));
-                        temp2=Integer.parseInt(line.substring(line.indexOf(' ')+1));
-                        ensembleSommet.get(temp1).rajouterSommet(ensembleSommet.get(temp2));
-                        if(!oriente)
-                            ensembleSommet.get(temp2).rajouterSommet(ensembleSommet.get(temp1));
+                    if(compteurLigne > 7+nbSommets+1 && compteurLigne < 7+ nbSommets +2+nbArcs) {
+                        // On trouve l'espace entre les 2 chiffres
+                        temp1 = Integer.parseInt(line.substring(0, line.indexOf(' ')));
+                        temp2 = Integer.parseInt(line.substring(line.indexOf(' ') + 1));
+                        sommets.get(temp1).rajouterSommet(sommets.get(temp2));
+                        if (!oriente) sommets.get(temp2).rajouterSommet(sommets.get(temp1));
                     }
-
                 }
-
+                listeAdjacence();
+                br.close();
+                in.close();
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                if (br != null) br.close();
-                if (in != null) in.close();
-
             }
-
     }
 
-    @Override
-    public String toString() {
-        return "Graphe{" +
-                "ensembleSommet=" + ensembleSommet +
-                ", nom='" + nom + '\'' +
-                ", oriente=" + oriente +
-                ", nbSommet=" + nbSommet +
-                ", nbValSommet=" + nbValSommet +
-                ", nbArcs=" + nbArcs +
-                ", nbValArcs=" + nbValArcs +
-                '}';
-    }
-    //On ordonne les sommets en fonction de leur degré
+    // On ordonne les sommets en fonction de leur degré
     public void ordonnerSommets(){
-        /*Collections.sort(ensembleSommet, new Comparator<Sommet>() {
+        /*Collections.sort(sommets, new Comparator<Sommet>() {
             @Override
             public int compare(Sommet s1,Sommet s2){
                 if( s1.degre()>s2.degre()) return 1;
@@ -134,6 +95,24 @@ public class Graphe {
                 }
             }
         });*/
-        ensembleSommet.sort(Comparator.comparing(Sommet::degre));
+        sommets.sort(Comparator.comparing(Sommet::degre));
     }
+
+    public void listeAdjacence() {
+        System.out.println("Liste d'adjacence du graphe " + this + " :");
+        for (Sommet s : sommets) {
+            if (s.degre() != 0) {
+                System.out.print(s + " ->");
+                for (Sommet arete : s.getAretes()) {
+                    System.out.print(" " + arete);
+                }
+                System.out.print("\n");
+            }
+        }
+        System.out.println("\n");
+    }
+
+    public String toString() { return nom;}
+
+    public List<Sommet> getSommets() { return sommets; }
 }
--- src/test.java
+++ src/test.java
@@ -1,13 +1,25 @@
 import java.io.IOException;
+import java.util.function.Consumer;
 
 public class test {
+
+    public static void clocking(Graphe g, Consumer<Graphe> method) {
+        long debut = System.nanoTime();
+        method.accept(g);
+        long fin = System.nanoTime();
+        System.out.println("Temps d'exécution de la méthode : " + (fin - debut)/1000000 + " ms");
+    }
+
     public static void main(String[] args) {
         Graphe g = new Graphe();
-        try {
-            g.lectureGraphe("crown10.txt");
-            // g.ordonnerSommets();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        g.lectureGraphe("crown10.txt");
+        // clocking(g, x -> x.lectureGraphe("crown10.txt"));
+        // g.ordonnerSommets();
     }
+
+    /* NOTES POUR MON JUAN
+    Pour chronométrer le temps que met "maMethode" à s'exécuter sur le graphe "monGraphe" :
+    clocking(monGraphe, x -> x.maMethode());
+    Tu peux mettre 'x' comme tu peux mettre 'y', 'z' ou 'nazi' : on s'en fout totalement, c'est une pure variable muette. <3
+     */
 }
