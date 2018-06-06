dadebc7c1af6bf01b197119e721f257ac3af54ed
DylanMarsangy <dylanmarsangy@outlook.fr>
2018-06-06 21:19:42 +0200

    Liste d'adjacence dispo

--- src/test.java
+++ src/test.java
@@ -1,17 +1,11 @@
-import com.sun.corba.se.impl.orbutil.graph.Graph;
-
 import java.io.IOException;
 
 public class test {
     public static void main(String[] args) {
-        Graphe g=new Graphe("hfeuhbhuebviurhv");
-        String line;
+        Graphe g = new Graphe();
         try {
             g.lectureGraphe("crown10.txt");
-            //System.out.println(g);
-            g.ordonnerSommets();
-            System.out.println(g);
-
+            // g.ordonnerSommets();
         } catch (IOException e) {
             e.printStackTrace();
         }
