119ed381f247cea40ff5578870424a995d4996e3
DylanMarsangy <dylanmarsangy@outlook.fr>
2018-06-10 19:18:30 +0200

    Meilleur affichage pour getColoration()

--- src/test.java
+++ src/test.java
@@ -1,29 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Consumer;
 import java.util.List;
 
 public class test {
 
-    public static void clocking(Graphe g, Consumer<Graphe> method) {
+    public static long clocking(Graphe g, Consumer<Graphe> method) {
         long debut = System.nanoTime();
         method.accept(g);
         long fin = System.nanoTime();
-        System.out.println("Temps d'exécution de la méthode : " + (fin - debut)/1000000 + " ms");
+
+        long tempsMicro = (fin - debut)/1000;
+        long tempsNano = fin - debut;
+        //System.out.println("Temps d'exécution de la méthode : " + tempsNano + " ns -> " + tempsMicro + " µs -> " + tempsMicro/1000 + " ms.\n");
+        return tempsMicro;
+    }
+
+    public static long moyenneClocking(Graphe g, Consumer<Graphe> method, int nbEchantillons) {
+        long temps = 0;
+        long moyenne;
+        for (int i = 0; i < nbEchantillons; i++) {
+            temps =+ clocking(g, method);
+        }
+        moyenne = temps / nbEchantillons;
+        System.out.println("Moyenne de temps d'éxécution : " + temps + " µs.");
+        return moyenne;
     }
 
     public static void main(String[] args) {
         Graphe g = new Graphe();
         g.lectureGraphe("crown10.txt");
-        List<Sommet> temp;
-        temp=g.ordonnerSommets();
 
-
-        g.greedyColoring();
-        g.reinitialiserCouleur();
-        g.welshPowell();
-        clocking(g,x-> x.Dsatur());
-
-        // clocking(g, x -> x.lectureGraphe("crown10.txt"));
-        // g.ordonnerSommets();
+        List<Consumer<Graphe>> algos = new ArrayList<>(Arrays.asList(Graphe::greedyColoring, Graphe::welshPowell, Graphe::Dsatur));
+        for (Consumer<Graphe> c : algos) {
+            moyenneClocking(g, c, 10);
+        }
     }
 
     /* NOTES POUR MON JUAN
@@ -31,5 +42,6 @@ public class test {
     clocking(monGraphe, x -> x.maMethode());
     Tu peux mettre 'x' comme tu peux mettre 'y', 'z' ou 'nazi' : on s'en fout totalement, c'est une pure variable muette. <3
      */
+
 
 }
